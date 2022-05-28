f90324cbdc5783317dfb879293029d3723fe8835
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-05-28 21:59:05 +0300

    Konkov

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,63 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <cassert>
+template <typename T>
+  class BST {
+   public:
+    struct Vertex {
+      T value;
+      int uni;
+      Vertex* left;
+      Vertex* right;
+    };
+  private:
+    Vertex* root;
+    Vertex* addVertex(Vertex* root, T value) {
+      if (root == nullptr) {
+        root = new Vertex;
+        root->value = value;
+        root->uni = 1;
+        root->left = root->right = nullptr;
+      } else if (root->value > value) {
+        root->left = addVertex(root->left, value);
+      } else if (root->value < value) {
+        root->right = addVertex(root->right, value);
+      } else {
+        root->uni++;
+      }
+      return root;
+    }
+    int deepTree(Vertex* root) {
+      if (root == nullptr)
+        return 0;
+      if (root->left == nullptr && root->right == nullptr)
+        return 0;
+      int but = deepTree(root->left), lat = deepTree(root->right);
+      return (but > lut ? but : lut) + 1;
+    }
+    int searchNode(Vertex* root, T value) {
+      if (root == nullptr)
+        return 0;
+      else if (root->value > value)
+        return searchNode(root->left, value);
+      else if (root->value < value)
+        return searchNode(root->right, value);
+      else
+        return root->uni;
+    }
+  public:
+    BST() :root(nullptr) {}
+    ~BST() {}
+    void add(T value) {
+      root = addVertex(root, value);
+    }
+    int deep() {
+      return deepTree(root);
+    }
+    int search(T value) {
+      return searchVertex(root, value);
+    }
+  };
 
 #endif  // INCLUDE_BST_H_
