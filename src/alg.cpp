7ee15cb3cbded24e28a339e5941759ddf5b761be
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-05-28 22:34:04 +0300

    Konkov

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,26 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string>tree;
+  std::string word = "";
+  char lc = 'a' - 'A';
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Error!" << std::endl;
+    return tr;
+  }
+  while (!file.eof()) {
+    char mr = file.get();
+    if ((mr >= 'a' && mr <= 'z') || (mr >= 'A' && mr <= 'Z')) {
+      if (mr >= 'A' && mr <= 'Z')
+        mr += lc;
+      word += mr;
+    } else if (word != "") {
+      tr.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return tr;
 }
